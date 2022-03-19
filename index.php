4b5a705e37589322878b90d616bde48c4896e736
Nancy <nancy62924@gmail.com>
2022-03-19 23:40:57 +0800

    add form layout

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
+<?php
+  session_start();
+
+  //Get Heroku ClearDB connection information
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $cleardb_server = $cleardb_url["host"];
+  $cleardb_username = $cleardb_url["user"];
+  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_db = substr($cleardb_url["path"],1);
+  $active_group = 'default';
+  $query_builder = TRUE;
+  // Connect to DB
+  $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>MIS 233｜Lab2</title>
+    <title>Sign up</title>
     <!-- CSS Style -->
-    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <!-- google fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -16,40 +30,55 @@
     />
   </head>
   <body>
-    <div id="top">
-      <h1>MIS 233.01</h1>
-      <p>Web Based Application Programming</p>
-    </div>
-    <div class="wrapper">
-      <!-- include -->
-      <?php
-        include "menu.php";
-        ?>
-      <div id="img-style">
-        <img src="./img/hiking.JPG" alt="hiking" width="155px" />
-      </div>
+    <?php
+      if(isset($_POST["submit"])){
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $upassword = $_POST["upassword"];
+        $upassword2 = $_POST["upassword2"];
 
-      <div id="main">
-        <h1>MIS 233.01 Home Page</h1>
+        if($upassword != $upassword2){
+          echo "passwords don't match";
+        }
+        else{
+        $db = new mysqli("us-cdbr-east-05.cleardb.net", "ba8a20c077011f", "a2f82246", "heroku_348200725298d23");
+        $result = $db -> query("INSERT INTO users(firstName, lastName, upassword) VALUES('$firstName', '$lastName', '$upassword')");
+
+        echo "Hello $firstName, you successfully created your account!";
+        }
+      }
+    ?>
+    <main class="card-wrapper">
+      <div class="card-container">
+        <h2>Sign up with your email</h2>
+        <h4>Already have an account?<span><a href="login.php" target="_blank">&nbsp;Sign in</a></span></h4>
+        <form action="index.php" method="post">
+          <div class="input-wrapper">
+            <div class="input-container">
+              <label for="">First Name</label>
+              <input type="text" name="firstName"/>
+            </div>
+            <div class="input-container">
+              <label for="">Last Name</label>
+              <input type="text" name="lastName"/>
+            </div>
+          </div>
+          <div class="input-container full-width-input">
+            <label for="">Email</label>
+            <input type="email" required name="email"/>
+          </div>
+          <div class="input-container full-width-input">
+            <label for="">Password</label>
+            <input type="password" name="upassword"/>
+          </div>
+          <div class="input-container full-width-input">
+            <label for="">Confirm Password</label>
+            <input type="password" name="upassword2"/>
+          </div>
+          <button name="submit">Create Account</button>
+        </form>
       </div>
-      <div id="ann">
-        <h3>Announcements</h3>
-        <p><a href="login.php" target="_blank">Login</a></p>
-        <p><a href="registration.php" target="_blank">Registration</a></p>
-      </div>
-    </div>
-    <div id="bottom">
-      <table id="category">
-        <tr>
-          <td class="cellitem">
-            &nbsp;&nbsp;&nbsp;&nbsp; Company&nbsp;&nbsp;&nbsp;&nbsp;
-          </td>
-          <td class="cellitem">&nbsp;&nbsp;About&nbsp;&nbsp;</td>
-          <td class="cellitem">&nbsp;&nbsp;Mission&nbsp;&nbsp;</td>
-          <td class="cellitem">&nbsp;&nbsp;Comment&nbsp;&nbsp;</td>
-        </tr>
-      </table>
-      <p>MIS 233.01 - Web Based Application Programming</p>
-    </div>
+    </main>
+    <p><a href="registration.php" target="_blank">Registration</a></p>
   </body>
 </html>
